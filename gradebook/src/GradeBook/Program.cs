5b3972473ba98932cb5b5a4efa21c8b48f7f4d2e
kelcc <kelcc169@yahoo.com>
2019-10-23 18:40:38 -0700

    learning c#

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double[] numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
+
+      var result = numbers[0];
+      result += numbers[1];
+      result += numbers[2];
+      Console.WriteLine(result);
+
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello, {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello");
+      }
+    }
+  }
+}
