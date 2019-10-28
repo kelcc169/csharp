53d507163838bd9768108508d46c8dace5e9847c
kelcc <kelcc169@yahoo.com>
2019-10-28 11:00:07 -0700

    next bit of lesson

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,9 +8,11 @@ namespace GradeBook
     {
       double[] numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
 
-      var result = numbers[0];
-      result += numbers[1];
-      result += numbers[2];
+      var result = 0.0;
+      foreach(double number in numbers)  
+      {
+        result += number;
+      }
       Console.WriteLine(result);
 
       if (args.Length > 0)
